6687c32db4a1e4b495366da196d617fd04359513
Prasad_Hp <itsprasadhp@gmail.com>
2024-07-29 08:21:11 +0000

    Login page created

---
+++ client/src/pages/Login.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { TextField, Button, Typography } from '@mui/material';
+import '@fontsource/roboto/500.css';
+
+function Login() {
+  return (
+    <div className='w-screen h-screen flex items-center justify-center'>
+        <form action="login" className='p-3 py-5 flex flex-col w-1/4 space-y-3 text-center bg-gray-300 rounded-lg border-gray-400 border-2 shadow-lg'>
+            <Typography variant="h3" gutterBottom>
+                Login
+            </Typography>
+            <TextField id="outlined-basic" label="Email" variant="outlined" className='bg-white rounded-md'/>
+            <TextField id="outlined-basic" label="Password" variant="outlined" className='bg-white rounded-md'/>
+            <Button variant="outlined" className='bg-blue-100 h-12'>Login</Button>
+        </form>
+
+    </div>
+  )
+}
+
+export default Login;
