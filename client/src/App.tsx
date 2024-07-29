d47a30d4e596ba0df6b817389115428f553b1ea2
Prasad_Hp <itsprasadhp@gmail.com>
2024-07-29 12:04:28 +0000

    Created Signup page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
 import Login from './pages/Login'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Signup from './pages/Signup'
+import Landing from './pages/Landing'
 
 function App() {
-
-
   return (
     <>
-      <Login />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/signup' element={<Signup />}/>
+        <Route path='/' element={<Landing />}/>
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
---
+++ client/src/pages/Signup.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { TextField, Button, Typography } from '@mui/material';
+import '@fontsource/roboto/500.css';
+
+function Signup() {
+  return (
+    <div className='w-screen h-screen flex items-center justify-center'>
+        <form action="login" className='p-3 py-5 flex flex-col w-1/4 space-y-3 text-center bg-gray-300 rounded-lg border-gray-400 border-2 shadow-lg'>
+            <Typography variant="h3" gutterBottom>
+                Signup
+            </Typography>
+            <TextField id="outlined-basic" label="First Name" variant="outlined" className='bg-white rounded-md'/>
+            <TextField id="outlined-basic" label="Last Name" variant="outlined" className='bg-white rounded-md'/>
+            <TextField id="outlined-basic" label="Email" type='email' variant="outlined" className='bg-white rounded-md'/>
+            <TextField id="outlined-basic" label="Password" type='password' variant="outlined" className='bg-white rounded-md'/>
+            <TextField id="outlined-basic" label="Confirm Password" type='password' variant="outlined" className='bg-white rounded-md'/>
+            <Button variant="outlined" className='bg-blue-100 h-12'>Create Account</Button>
+        </form>
+    </div>
+  )
+}
+
+export default Signup
