3a747c938c2c2272c38639ce6ffa58bda11bda22
Prasad_Hp <itsprasadhp@gmail.com>
2024-08-06 07:47:47 +0000

    Updated naviagation nav bar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,19 +1,21 @@
 import { useState } from 'react'
 import { Button } from '@mui/material';
+import { Navigate, useNavigate } from 'react-router-dom';
 function Navbar() {
     const [logged, setLogged] = useState(false)
+    const navigate = useNavigate();
 
   return (
     <div className='flex justify-between w-full p-3'>
-        <div className='flex items-center space-x-1'>
+        <div className='flex items-center space-x-1' onClick={()=>navigate("/")}>
             <img src="../openai.svg" alt="Logo OpenAi" className='h-8'/>
             <p className='text-2xl font-sans font-semibold'>GPT</p>
         </div>
         <div>
             <div className= {`${ logged ? "hidden" : "inline-block"} `}>
                 <div className='flex space-x-2'>
-                    <Button variant="contained" color='success' onClick={()=>setLogged(!logged)}>Login</Button>
-                    <Button variant="contained" color='secondary' onClick={()=>setLogged(!logged)}>Signup</Button>
+                    <Button variant="contained" color='success' onClick={()=>navigate("/login")}>Login</Button>
+                    <Button variant="contained" color='secondary' onClick={()=>navigate("/signup")}>Signup</Button>
                 </div>
             </div>
             <div className={`${ logged ? "inline-block" : "hidden" }`}>
